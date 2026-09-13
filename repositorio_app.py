1a6ab37b96825e8e8cfca76ccac28c6529304a14
naylinbeleno03-dev <naylinbeleno03@gmail.com>
2026-09-13 18:15:30 -0500

    Update repositorio_app.py

--- repositorio_app.py
+++ repositorio_app.py
@@ -122,15 +122,16 @@ st.markdown(
 )
 
 # Configuración de Google Sheets
-SHEET_ID = "1eUTG3EFoVvRDpycgNv6JdUP_jAa4106SCCNhXIQGFsc"  # Reemplaza con el ID de tu Google Sheet
+SHEET_ID = "1eUTG3EFoVvRDpycgNv6JdUP_jAa4106SCCNhXIQGFsc"
 url_csv = f"https://docs.google.com/spreadsheets/d/{SHEET_ID}/export?format=csv"
-URL_APPS_SCRIPT = (
-    "https://script.google.com/macros/s/AKfycbzyoa-yvvfMj3RE94dGU9tSaPLbo0fRcQs5cfp_QuiB5yUYphukHOj104WWFJ11iSctgQ/exec"  # Reemplaza con la URL de la aplicación web desplegada
-)
+URL_APPS_SCRIPT = "https://script.google.com/macros/s/AKfycbzyoa-yvvfMj3RE94dGU9tSaPLbo0fRcQs5cfp_QuiB5yUYphukHOj104WWFJ11iSctgQ/exec"
 
-# Cargar datos directamente desde Google Sheets de forma segura
+# Cargar datos directamente desde Google Sheets de forma segura y normalizada
 try:
   df = pd.read_csv(url_csv)
+  # Limpiar y convertir nombres de columnas a minúsculas para evitar duplicados
+  df.columns = [str(col).strip().lower() for col in df.columns]
+
   if "archivo" not in df.columns:
     df["archivo"] = "Sin archivo"
 except Exception:
